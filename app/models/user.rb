cbaefe43526abcab3a9538450496fa8f332a0bd0
山田　潤 <junyamada107@gmail.com>
2015-01-07 17:26:41 +0900

    no message

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :username, presence: true, uniqueness: true
-  validates :nature, presence: true, uniqueness: true
+  validates :nature, presence: true
 end
